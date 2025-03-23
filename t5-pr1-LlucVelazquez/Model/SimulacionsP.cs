b02f81c3051ca2967b9e9f572caa51dec3d07c99
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-03-23 13:16:52 +0100

    Create Simulations test

---
+++ t5-pr1-LlucVelazquez/Model/SimulacionsP.cs
@@ -0,0 +1,22 @@
+﻿namespace t5_pr1_LlucVelazquez.Model
+{
+    public enum TypeSim
+    {
+        SolarSystem,
+        EolicSystem,
+        HidroelectricSystem
+    }
+    public class SimulacionsP
+    {
+        public int Id { get; set; } 
+        public TypeSim Type { get; set; }
+        public double HoresSol { get; set; }
+        public double VelocitatVent { get; set; }
+        public double CabalAigua { get; set; }
+        public double Rati { get; set; }
+        public double EnergyGen { get; set; }
+        public decimal CostTotal { get; set; }
+        public decimal PreuTotal { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
