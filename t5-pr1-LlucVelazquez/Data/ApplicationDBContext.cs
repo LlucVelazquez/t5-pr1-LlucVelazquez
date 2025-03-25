1a39a0d9918628eab5aeb39fe92c481cc47b6edd
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-03-25 18:33:34 +0100

    Modified AppDBContext

--- t5-pr1-LlucVelazquez/Data/ApplicationDBContext.cs
+++ t5-pr1-LlucVelazquez/Data/ApplicationDBContext.cs
@@ -6,11 +6,17 @@ namespace t5_pr1_LlucVelazquez.Data
 	public class ApplicationDBContext : DbContext
 	{
 		public DbSet<Simulation> Simulations { get; set; }
-		public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
-
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			
-		}
-	}
+        public DbSet<EnergyIndicator> EnergyIndicators { get; set; }
+		public DbSet<WaterConsume> WaterConsumes { get; set; }
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            string connectionString = config.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
 }
