6683c6da21cc99b8bd29fa46735df252d65e34c8
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-03-26 13:07:11 +0100

    no va

--- t5-pr1-LlucVelazquez/Migrations/20250326120529_Initial2.Designer.cs
+++ t5-pr1-LlucVelazquez/Migrations/20250326120529_Initial2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using t5_pr1_LlucVelazquez.Data;
 
@@ -11,9 +12,11 @@ using t5_pr1_LlucVelazquez.Data;
 namespace t5_pr1_LlucVelazquez.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250326120529_Initial2")]
+    partial class Initial2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
